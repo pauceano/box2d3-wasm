04b37ca86af4fcf1000092c0f205fd5d0b809cb3
Birch-san <alex@anlatan.ai>
2024-12-08 22:04:21 +0000

    compile wasm from bare wasm

--- box2d3-wasm/shell/1_build_wasm.sh
+++ box2d3-wasm/shell/1_build_wasm.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
-# FLAVOUR=simd TARGET_TYPE=Debug ./shell/1_build_wasm.sh
+# FLAVOUR=simd TARGET_TYPE=Debug BUILD_UMD_VIA_TEXT_REPLACE=1 ./shell/1_build_wasm.sh
 set -eo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-BOX2D_DIR="$(realpath "$DIR/../../box2d")"
+B2D_WASM_DIR="$(realpath "$DIR/..")"
+MONOREPO_DIR="$(realpath "$B2D_WASM_DIR/..")"
+BOX2D_DIR="$(realpath "$MONOREPO_DIR/box2d")"
+B2CPP_DIR="$(realpath "$MONOREPO_DIR/box2cpp")"
+CMAKEBUILD_DIR="$(realpath "$B2D_WASM_DIR/cmake-build")"
+BUILD_DIR="$(realpath "$B2D_WASM_DIR/build")"
 
 Red='\033[0;31m'
 Green='\033[0;32m'
@@ -33,9 +38,8 @@ EMCC_OPTS=(
   -s MODULARIZE=1
   -s EXPORT_NAME=Box2D
   -s ALLOW_TABLE_GROWTH=1
-  --memory-init-file 0
   -s FILESYSTEM=0
-  -s SUPPORT_LONGJMP=0
+  # -s SUPPORT_LONGJMP=0 # this causes 'undefined symbol: _emscripten_stack_restore'
   -s EXPORTED_FUNCTIONS=_malloc,_free
   -s ALLOW_MEMORY_GROWTH=1
   ${FLAVOUR_EMCC_OPTS[@]}
@@ -51,7 +55,7 @@ case "$TARGET_TYPE" in
       ${EMCC_OPTS[@]}
       ${DEBUG_OPTS[@]}
       -s ASSERTIONS=2
-      -s DEMANGLE_SUPPORT=1
+      # -s DEMANGLE_SUPPORT=1 # deprecated
       )
     ;;
 
@@ -84,6 +88,61 @@ case "$TARGET_TYPE" in
 esac
 >&2 echo -e "TARGET_TYPE is $TARGET_TYPE"
 
-BARE_WASM="$BASENAME.bare.wasm"
+mkdir -p "$BUILD_DIR"
+BARE_WASM="$BUILD_DIR/$BASENAME.bare.wasm"
 
 >&2 echo -e "${Blue}Building bare WASM${NC}"
+# emcc "$DIR/glue_stub.cpp" bin/libbox2d.a -I "$DIR/../box2d/include" "${EMCC_OPTS[@]}" --oformat=bare -o "$BARE_WASM"
+# emcc "$CMAKEBUILD_DIR/src/libbox2dd.a" -I "$BOX2D_DIR/include" "${EMCC_OPTS[@]}" --oformat=bare -o "$BARE_WASM"
+emcc "$CMAKEBUILD_DIR/src/libbox2dd.a" -I "$BOX2D_DIR/include" "${EMCC_OPTS[@]}" --oformat=bare -o "$BARE_WASM"
+
+UMD_DIR="$BUILD_DIR/dist/umd"
+ES_DIR="$BUILD_DIR/dist/es"
+
+mkdir -p "$UMD_DIR" "$ES_DIR"
+
+>&2 echo -e "${Blue}Building post-link targets${NC}"
+
+# LINK_OPTS=(--post-link "$BARE_WASM" --post-js "$DIR/build/common/box2d_glue.js" --post-js "$DIR/glue_stub.js" ${EMCC_OPTS[@]})
+LINK_OPTS=(--post-link "$BARE_WASM")
+
+ES_FILE="$ES_DIR/$BASENAME.js"
+>&2 echo -e "${Blue}Building ES module, $ES_DIR/$BASENAME.{js,wasm}${NC}"
+set -x
+emcc "${LINK_OPTS[@]}" -s EXPORT_ES6=1 -o "$ES_FILE"
+{ set +x; } 2>&-
+>&2 echo -e "${Green}Successfully built $ES_DIR/$BASENAME.{js,wasm}${NC}\n"
+
+UMD_FILE="$UMD_DIR/$BASENAME.js"
+# cheeky text-replace to save time.
+# only works if the text-substitution is exactly as we expected (so may fail silently depending on Emscripten version or config)
+if [ "$BUILD_UMD_VIA_TEXT_REPLACE" = "1" ]; then
+  >&2 echo -e "${Blue}Building UMD module, $UMD_DIR/$BASENAME.{js,wasm} by replacing header & footer of ES module${NC}"
+  escape_for_sed_replace () {
+    echo "$1" | sed -e 's/&/\\\&/g' -e '$!s/$/\\n/' | tr -d '\n'
+  }
+
+  ES6_HEADER='  var _scriptDir = import.meta.url;'
+  UMD_HEADER="  var _scriptDir = typeof document !== 'undefined' && document.currentScript ? document.currentScript.src : undefined;
+  if (typeof __filename !== 'undefined') _scriptDir = _scriptDir || __filename;"
+  UMD_HEADER_ESCAPED=`escape_for_sed_replace "$UMD_HEADER"`
+
+  ES6_FOOTER='export default Box2D;'
+  UMD_FOOTER="if (typeof exports === 'object' && typeof module === 'object')
+      module.exports = Box2D;
+    else if (typeof define === 'function' && define['amd'])
+      define([], function() { return Box2D; });
+    else if (typeof exports === 'object')
+      exports['Box2D'] = Box2D;
+    "
+  UMD_FOOTER_ESCAPED=`escape_for_sed_replace "$UMD_FOOTER"`
+
+  sed -e "s/^$ES6_HEADER$/$UMD_HEADER_ESCAPED/" -e "s/^$ES6_FOOTER$/$UMD_FOOTER_ESCAPED/" "$ES_FILE" > "$UMD_FILE"
+  cp "$ES_DIR/$BASENAME.wasm" "$UMD_DIR"
+else
+  >&2 echo -e "${Blue}Building UMD module, $UMD_DIR/$BASENAME.{js,wasm} from scratch${NC}"
+  set -x
+  emcc "${LINK_OPTS[@]}" -o "$UMD_FILE"
+  { set +x; } 2>&-
+fi
+>&2 echo -e "${Green}Successfully built $UMD_DIR/$BASENAME.{js,wasm}${NC}\n"
