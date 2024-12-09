7735964ed7d2c929ef318c9d9da31492be6101cb
Erik Sombroek <eriksombroek@gmail.com>
2024-12-09 22:12:43 +0100

    initial bindings for shape

--- box2d3-wasm/csrc/glue.cpp
+++ box2d3-wasm/csrc/glue.cpp
@@ -9,19 +9,40 @@ using namespace b2;
 // EMSCRIPTEN_DECLARE_VAL_TYPE(b2TaskCallback)
 // EMSCRIPTEN_DECLARE_VAL_TYPE(b2EnqueueTaskCallback)
 
+
+emscripten::val b2ShapeDef_getUserData(const b2ShapeDef& self) {
+    return emscripten::val(reinterpret_cast<std::uintptr_t>(self.userData));
+}
+void b2ShapeDef_setUserData(b2ShapeDef& self, const emscripten::val& value) {
+    self.userData = reinterpret_cast<void*>(value.as<std::uintptr_t>());
+}
+
+static double b2Filter_getCategoryBits(const b2Filter &f) {
+    return (double)f.categoryBits;
+}
+static void b2Filter_setCategoryBits(b2Filter &f, double val) {
+    f.categoryBits = (uint64_t)val;
+}
+static double b2Filter_getMaskBits(const b2Filter &f) {
+    return (double)f.maskBits;
+}
+static void b2Filter_setMaskBits(b2Filter &f, double val) {
+    f.maskBits = (uint64_t)val;
+}
+
 EMSCRIPTEN_BINDINGS(box2d) {
     class_<b2Vec2>("b2Vec2")
         .constructor()
         .property("x", &b2Vec2::x)
         .property("y", &b2Vec2::y)
         ;
-    
+
     class_<b2CosSin>("b2CosSin")
         .constructor()
         .property("cosine", &b2CosSin::cosine)
         .property("sine", &b2CosSin::sine)
         ;
-    
+
     class_<b2Rot>("b2Rot")
         .constructor()
         .property("c", &b2Rot::c)
@@ -39,13 +60,13 @@ EMSCRIPTEN_BINDINGS(box2d) {
         .property("cx", &b2Mat22::cx, return_value_policy::reference())
         .property("cy", &b2Mat22::cy, return_value_policy::reference())
         ;
-    
+
     class_<b2AABB>("b2AABB")
         .constructor()
         .property("lowerBound", &b2AABB::lowerBound, return_value_policy::reference())
         .property("upperBound", &b2AABB::upperBound, return_value_policy::reference())
         ;
-    
+
     enum_<b2MixingRule>("b2MixingRule")
         .value("b2_mixAverage", b2MixingRule::b2_mixAverage)
         .value("b2_mixGeometricMean", b2MixingRule::b2_mixGeometricMean)
@@ -77,12 +98,117 @@ EMSCRIPTEN_BINDINGS(box2d) {
         // .property("userData", &b2WorldDef::userData, allow_raw_pointers())
         .property("internalValue", &b2WorldDef::internalValue)
         ;
-    
+
     // reference return policy doesn't work here
     function("b2DefaultWorldDef", &b2DefaultWorldDef);
 
     class_<b2::World>("World")
         .constructor()
         .constructor<const b2WorldDef&>()
+        ;
+
+     class_<b2Circle>("b2Circle")
+        .constructor()
+        .property("center", &b2Circle::center)
+        .property("radius", &b2Circle::radius)
+        ;
+
+    class_<b2Capsule>("b2Capsule")
+        .constructor()
+        .property("center1", &b2Capsule::center1)
+        .property("center2", &b2Capsule::center2)
+        .property("radius", &b2Capsule::radius)
+        ;
+
+    class_<b2Segment>("b2Segment")
+        .constructor()
+        .property("point1", &b2Segment::point1)
+        .property("point2", &b2Segment::point2)
+        ;
+
+    class_<b2Filter>("b2Filter")
+        .constructor()
+        .property("categoryBits", &b2Filter_getCategoryBits, &b2Filter_setCategoryBits)
+        .property("maskBits", &b2Filter_getMaskBits, &b2Filter_setMaskBits)
+        .property("groupIndex", &b2Filter::groupIndex);
+
+    class_<b2ShapeDef>("b2ShapeDef")
+        .constructor()
+        .property("userData", &b2ShapeDef_getUserData, &b2ShapeDef_setUserData)
+        .property("friction", &b2ShapeDef::friction)
+        .property("restitution", &b2ShapeDef::restitution)
+        .property("density", &b2ShapeDef::density)
+        .property("filter", &b2ShapeDef::filter)
+        .property("customColor", &b2ShapeDef::customColor)
+        .property("isSensor", &b2ShapeDef::isSensor)
+        .property("enableSensorEvents", &b2ShapeDef::enableSensorEvents)
+        .property("enableContactEvents", &b2ShapeDef::enableContactEvents)
+        .property("enableHitEvents", &b2ShapeDef::enableHitEvents)
+        .property("enablePreSolveEvents", &b2ShapeDef::enablePreSolveEvents)
+        .property("invokeContactCreation", &b2ShapeDef::invokeContactCreation)
+        .property("updateBodyMass", &b2ShapeDef::updateBodyMass)
+        ;
+
+    class_<b2Polygon>("b2Polygon")
+        .constructor()
+        .function("getVertex", +[](const b2Polygon& self, int index) -> b2Vec2 {
+            if (index < 0 || index >= b2_maxPolygonVertices) return b2Vec2();
+            return self.vertices[index];
+        })
+        .function("setVertex", +[](b2Polygon& self, int index, const b2Vec2& value) {
+            if (index >= 0 && index < b2_maxPolygonVertices) {
+                self.vertices[index] = value;
+            }
+        })
+        .function("getNormal", +[](const b2Polygon& self, int index) -> b2Vec2 {
+            if (index < 0 || index >= b2_maxPolygonVertices) return b2Vec2();
+            return self.normals[index];
+        })
+        .function("setNormal", +[](b2Polygon& self, int index, const b2Vec2& value) {
+            if (index >= 0 && index < b2_maxPolygonVertices) {
+                self.normals[index] = value;
+            }
+        })
+        .property("centroid", &b2Polygon::centroid)
+        .property("radius", &b2Polygon::radius)
+        .property("count", &b2Polygon::count)
+        .class_function("getMaxVertices", +[]() { return b2_maxPolygonVertices; })
+    ;
+
+    function("b2MakeBox", &b2MakeBox);
+    function("b2MakeSquare", &b2MakeSquare);
+    function("b2MakeRoundedBox", &b2MakeRoundedBox);
+    function("b2MakeOffsetBox", &b2MakeOffsetBox);
+    function("b2MakeOffsetRoundedBox", &b2MakeOffsetRoundedBox);
+
+    class_<BasicShapeInterface<Shape, false>>("BasicShapeInterface")
+        .function("destroy", &BasicShapeInterface<Shape, false>::Destroy)
+        .function("isValid", &BasicShapeInterface<Shape, false>::IsValid)
+        .function("getAABB", &BasicShapeInterface<Shape, false>::GetAABB)
+        .function("getDensity", &BasicShapeInterface<Shape, false>::GetDensity)
+        .function("setDensity", &BasicShapeInterface<Shape, false>::SetDensity)
+        .function("getFilter", &BasicShapeInterface<Shape, false>::GetFilter)
+        .function("setFilter", &BasicShapeInterface<Shape, false>::SetFilter)
+        .function("getFriction", &BasicShapeInterface<Shape, false>::GetFriction)
+        .function("setFriction", &BasicShapeInterface<Shape, false>::SetFriction)
+        ;
+
+    class_<Shape, base<BasicShapeInterface<Shape, false>>>("Shape")
+        .constructor<>()
+        .function("destroy", &Shape::Destroy)
+        .function("isValid", &Shape::IsValid)
+        .function("getAABB", &Shape::GetAABB)
+        .function("getDensity", &Shape::GetDensity)
+        .function("setDensity", &Shape::SetDensity)
+        .function("getFilter", &Shape::GetFilter)
+        .function("setFilter", &Shape::SetFilter)
+        .function("getFriction", &Shape::GetFriction)
+        .function("setFriction", &Shape::SetFriction)
+        .function("getUserData", +[](const Shape& self) {
+            return emscripten::val(static_cast<double>(reinterpret_cast<std::uintptr_t>(self.GetUserData())));
+        })
+        .function("setUserData", +[](Shape& self, const emscripten::val& value) {
+            self.SetUserData(reinterpret_cast<void*>(static_cast<std::uintptr_t>(value.as<double>())));
+        })
         ;
 }
--- box2d3-wasm/integration-test/index.mjs
+++ box2d3-wasm/integration-test/index.mjs
@@ -5,9 +5,87 @@ const {
     b2DefaultWorldDef,
     b2WorldDef,
     World,
+    b2MakeBox
 } = box2d;
 const B2_SECRET_COOKIE = 1152023
 const worldDef = new b2WorldDef(b2DefaultWorldDef())
 assert(worldDef.internalValue === B2_SECRET_COOKIE)
 const world = new World(worldDef)
 console.log('hello', world)
+
+const sideLengthMetres = 1;
+const square = b2MakeBox(sideLengthMetres, sideLengthMetres);
+console.log('square', square)
+
+
+/*
+import Box2DFactory from 'box2d-wasm';
+import { assertFloatEqual } from './assertFloatEqual';
+
+const {
+  b2BodyDef,
+  b2_dynamicBody,
+  b2PolygonShape,
+  b2Vec2,
+  b2World,
+  destroy,
+  getPointer,
+  LeakMitigator,
+  NULL
+}: typeof Box2D & EmscriptenModule = await Box2DFactory();
+
+const { freeFromCache } = LeakMitigator;
+const { recordLeak, freeLeaked } = new LeakMitigator();
+
+const gravity = new b2Vec2(0, 10);
+const world = new b2World(gravity);
+
+const sideLengthMetres = 1;
+const square = new b2PolygonShape();
+square.SetAsBox(sideLengthMetres/2, sideLengthMetres/2);
+
+const zero = new b2Vec2(0, 0);
+
+const bd = new b2BodyDef();
+bd.set_type(b2_dynamicBody);
+bd.set_position(zero);
+
+const body = recordLeak(world.CreateBody(bd));
+destroy(bd);
+freeFromCache(body.CreateFixture(square, 1));
+destroy(square);
+body.SetTransform(zero, 0);
+body.SetLinearVelocity(zero);
+destroy(zero);
+body.SetAwake(true);
+body.SetEnabled(true);
+
+const timeStepMillis = 1/60;
+const velocityIterations = 1;
+const positionIterations = 1;
+const floatCompareTolerance = 0.01;
+
+const iterations = 6;
+for (let i=0; i<iterations; i++) {
+  const timeElapsedMillis = timeStepMillis*i;
+  {
+    const {y} = body.GetLinearVelocity();
+    assertFloatEqual(y, gravity.y*timeElapsedMillis, floatCompareTolerance);
+    {
+      const {y} = body.GetPosition();
+      assertFloatEqual(y, 0.5*gravity.y*timeElapsedMillis**2, floatCompareTolerance);
+    }
+  }
+  world.Step(timeStepMillis, velocityIterations, positionIterations);
+}
+
+destroy(gravity);
+
+for (let body = recordLeak(world.GetBodyList()); getPointer(body) !== getPointer(NULL); body = recordLeak(body.GetNext())) {
+  world.DestroyBody(body);
+}
+
+destroy(world);
+freeLeaked();
+
+console.log(`👍 Ran ${iterations} iterations of a falling body. Body had the expected position on each iteration.`);*/
