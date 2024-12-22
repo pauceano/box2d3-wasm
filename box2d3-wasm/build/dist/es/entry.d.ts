af5d6448e114d491fb6e54f0e60f61a610c2cf6a
Birch-san <alex@anlatan.ai>
2024-12-22 03:19:32 +0000

    make typings a bit more discoverable by demo. still not surviving past the Promise#then though.

--- box2d3-wasm/build/dist/es/entry.d.ts
+++ box2d3-wasm/build/dist/es/entry.d.ts
@@ -1,4 +1,4 @@
 // these typings are provided in case you wish to bypass the NodeJS module specifier
 // (i.e. 'box2d-wasm') and import the entrypoint directly.
-declare const module: typeof import('./Box2D.simd');
-export default module;
+import type Box2D from './Box2D.simd'
+export = Box2D;
